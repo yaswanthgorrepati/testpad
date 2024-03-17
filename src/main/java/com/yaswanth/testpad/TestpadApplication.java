cb097ba0ec30b41d37569d11cfe998b22f66dfe4
YaswanthGorrepati <gorrepatiyaswanth2@gmail.com>
2024-03-17 07:36:48 +0530

    init setup

---
+++ src/main/java/com/yaswanth/testpad/TestpadApplication.java
@@ -0,0 +1,13 @@
+package com.yaswanth.testpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaswanth/testpad/TestpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaswanth.testpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
